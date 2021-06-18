92d8cb376664e3a79e5b253c4dc6d199dd3b0405
MisemiWang <85859814+MisemiWang@users.noreply.github.com>
2021-06-18 20:30:14 +0800

    Update read.py

--- read.py
+++ read.py
@@ -4,7 +4,7 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0
+        if count % 1000 == 0:
             print(len(data))
 print(len(data))
 
